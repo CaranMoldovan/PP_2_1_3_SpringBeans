3cf34e0d21cdc142d856cb3fadbd4223137a5874
CaranMoldovan <116651970+CaranMoldovan@users.noreply.github.com>
2024-02-18 15:31:17 +0300

    first Commit

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,12 +3,10 @@ package app.model;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Timer {
-
-    private Long nanoTime = System.nanoTime();
-
-    public Long getTime() {
-        return nanoTime;
+@Component("dog")
+public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "I'm a dog";
     }
 }
